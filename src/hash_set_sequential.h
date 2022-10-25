ce23685cc7df38903a235ceca5cb7f5291b54eb4
Jordan  Kisa <jordan.kisa18@imperial.ac.uk>
2022-10-25 18:56:43 +0100

    Implemented sequential hashset

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -2,31 +2,63 @@
 #define HASH_SET_SEQUENTIAL_H
 
 #include <cassert>
+#include <vector>
+#include <iostream>
 
 #include "src/hash_set_base.h"
 
 template <typename T> class HashSetSequential : public HashSetBase<T> {
 public:
-  explicit HashSetSequential(size_t /*initial_capacity*/) {}
+  explicit HashSetSequential(size_t initial_capacity) {
+    table = std::vector<std::vector<T>>(initial_capacity);
+    set_size = 0;
+  }
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
+  bool Add(T elem) final {
+    std::vector<T>& bucket = GetBucket(elem);
+    if (VectorContains(bucket, elem)) {
+      return false;
+    } else {
+      bucket.push_back(elem);
+      set_size++;
+      return true;
+    }
+  }
+
+  bool Remove(T elem) final {
+    std::vector<T>& bucket = GetBucket(elem);
+    for (auto it = bucket.begin(); it != bucket.end(); it++) {
+      if (*it == elem) {
+        bucket.erase(it);
+        set_size--;
+        return true;
+      }
+    }
     return false;
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
+  [[nodiscard]] bool Contains(T elem) final {
+    std::vector<T>& bucket = GetBucket(elem);
+    return VectorContains(bucket, elem);
+  }
+
+  [[nodiscard]] size_t Size() const final { return set_size; }
+
+private:
+  size_t set_size;
+  std::vector<std::vector<T>> table;
+
+  bool VectorContains(std::vector<T>& v, const T& elem) {
+    for (auto it = v.begin(); it != v.end(); it++) {
+      if (*it == elem) {
+        return true;
+      }
+    }
     return false;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
-
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+  std::vector<T>& GetBucket(T elem) {
+    return table[std::hash<T>()(elem) % table.size()];
   }
 };
 
