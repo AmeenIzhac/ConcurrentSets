a3478abf0ec3e2d11811aceb811a3055309dd4e5
Jordan  Kisa <jordan.kisa18@imperial.ac.uk>
2022-10-26 16:41:51 +0100

    Formatting sequential
    Co-authored-by: Ameen Izhac <AmeenIzhac@users.noreply.github.com>

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -6,70 +6,97 @@
 
 #include "src/hash_set_base.h"
 
-template <typename T> class HashSetSequential : public HashSetBase<T> {
+template <typename T>
+class HashSetSequential : public HashSetBase<T>
+{
 public:
-  explicit HashSetSequential(size_t initial_capacity) {
+  explicit HashSetSequential(size_t initial_capacity)
+  {
     table = std::vector<std::vector<T>>(initial_capacity);
-    set_size = 0;
+    set_size_ = 0;
   }
 
-  bool Add(T elem) final {
+  // Finds the bucket corresponding to the elems hash and inserts the element to that bucket
+  bool Add(T elem) final
+  {
     std::vector<T> &bucket = GetBucket(elem);
-    if (VectorContains(bucket, elem)) {
+    if (VectorContains(bucket, elem))
+    {
       return false;
-    } else {
+    }
+    else
+    {
       bucket.push_back(elem);
-      set_size++;
-      if (policy()) {
-        resize();
+      set_size_++;
+      if (Policy())
+      {
+        Resize();
       }
       return true;
     }
   }
 
-  bool Remove(T elem) final {
+  // Finds the bucket corresponding to the elems hash and removes the element from that bucket
+  bool Remove(T elem) final
+  {
     std::vector<T> &bucket = GetBucket(elem);
-    for (auto it = bucket.begin(); it != bucket.end(); it++) {
-      if (*it == elem) {
+    for (auto it = bucket.begin(); it != bucket.end(); it++)
+    {
+      if (*it == elem)
+      {
         bucket.erase(it);
-        set_size--;
+        set_size_--;
         return true;
       }
     }
     return false;
   }
 
-  [[nodiscard]] bool Contains(T elem) final {
+  // Returns true iffthe element is contained in the HashSet and false otherwise
+  [[nodiscard]] bool Contains(T elem) final
+  {
     std::vector<T> &bucket = GetBucket(elem);
     return VectorContains(bucket, elem);
   }
 
-  [[nodiscard]] size_t Size() const final { return set_size; }
+  // Returns the total amount of elements in hashset
+  [[nodiscard]] size_t Size() const final { return set_size_; }
 
 private:
-  size_t set_size;
+  size_t set_size_;
   std::vector<std::vector<T>> table;
 
-  bool VectorContains(std::vector<T> &v, const T &elem) {
-    for (auto it = v.begin(); it != v.end(); it++) {
-      if (*it == elem) {
+  // Helper to Contains returning true iff an element is contained in a bucket
+  bool VectorContains(std::vector<T> &v, const T &elem)
+  {
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+      if (*it == elem)
+      {
         return true;
       }
     }
     return false;
   }
 
-  std::vector<T> &GetBucket(T elem) {
+  // Returns corresponding bucket for elem based on it's hash
+  std::vector<T> &GetBucket(T elem)
+  {
     return table[std::hash<T>()(elem) % table.size()];
   }
 
-  bool policy() { return set_size / table.size(); }
+  // Average length of bucket is greater than 4
+  bool Policy() { return set_size_ / table.size() > 4; }
 
-  void resize() {
+  // Doubles bucket vector and puts elements into new buckets
+  void Resize()
+  {
     std::vector<std::vector<T>> old_table = table;
     table = std::vector<std::vector<T>>(old_table.size() * 2);
-    for (auto &bucket : old_table) {
-      for (auto &elem : bucket) {
+    for (auto &bucket : old_table)
+    {
+      for (auto &elem : bucket)
+      {
         GetBucket(elem).push_back(elem);
       }
     }
